b82d9e724e29a1a933b1728f8fc91b980f8e47ef
AlexeiAK <aak3xx@gmail.com>
2022-07-09 13:56:25 +0300

    refactor: UrlController and build.gradle

--- src/main/java/hexlet/code/controllers/UrlController.java
+++ src/main/java/hexlet/code/controllers/UrlController.java
@@ -12,6 +12,7 @@ import kong.unirest.core.Unirest;
 import kong.unirest.core.UnirestException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -44,32 +45,16 @@ public class UrlController {
         ctx.attribute("pages", pages);
         ctx.attribute("currentPage", currentPage);
 
-        ctx.render("urls/urls.html");
+        ctx.render("urls/index.html");
     };
 
 
     public static Handler addUrl = ctx -> {
-        String urlFromForm = ctx.formParam("url");
-        String hostName = null;
+        String inputUrl = ctx.formParam("url");
+        URL parsedUrl;
 
         try {
-            URL parsedUrl = new URL(urlFromForm);
-            hostName = parsedUrl.getProtocol() + "://" + parsedUrl.getAuthority();
-
-            Url url = new QUrl()
-                .name.equalTo(hostName)
-                .findOne();
-
-            if (url != null) {
-                ctx.sessionAttribute("flash", "Страница уже существует");
-                ctx.sessionAttribute("flash-type", "info");
-            } else {
-                Url newUrl = new Url(hostName);
-                newUrl.save();
-
-                ctx.sessionAttribute("flash", "Страница успешно добавлена");
-                ctx.sessionAttribute("flash-type", "success");
-            }
+            parsedUrl = new URL(inputUrl);
         } catch (MalformedURLException e) {
             ctx.sessionAttribute("flash", "Некорректный URL");
             ctx.sessionAttribute("flash-type", "danger");
@@ -77,6 +62,22 @@ public class UrlController {
             return;
         }
 
+        String hostName = parsedUrl.getProtocol() + "://" + parsedUrl.getAuthority();
+        Url url = new QUrl()
+            .name.equalTo(hostName)
+            .findOne();
+
+        if (url != null) {
+            ctx.sessionAttribute("flash", "Страница уже существует");
+            ctx.sessionAttribute("flash-type", "info");
+            return;
+        }
+
+        Url newUrl = new Url(hostName);
+        newUrl.save();
+
+        ctx.sessionAttribute("flash", "Страница успешно добавлена");
+        ctx.sessionAttribute("flash-type", "success");
         ctx.redirect("/urls");
     };
 
@@ -107,24 +108,18 @@ public class UrlController {
         Url url = new QUrl().id.equalTo(id).findOne();
 
         try {
-            HttpResponse<String> response = Unirest
-                .get(url.getName())
-                .asString();
-
-            Document body = Jsoup.parse(response.getBody());
+            HttpResponse<String> response = Unirest.get(url.getName()).asString();
+            Document doc = Jsoup.parse(response.getBody());
 
             int statusCode = response.getStatus();
-            String title = body.title();
+            String title = doc.title();
 
-            String description = null;
-            if (body.selectFirst("meta[name=description]") != null) {
-                description = body.selectFirst("meta[name=description]").attr("content");
-            }
+            Element descriptionElement = doc.selectFirst("meta[name=description]");
+            String description = descriptionElement == null ? "" : descriptionElement.attr("content");
 
-            String h1 = null;
-            if (body.selectFirst("h1") != null) {
-                h1 = body.selectFirst("h1").text();
-            }
+            Element h1Element = doc.selectFirst("h1");
+            String h1 = h1Element == null ? "" : h1Element.text();
+
 
             UrlCheck urlCheck = new UrlCheck(statusCode, title, h1, description, url);
             urlCheck.save();
